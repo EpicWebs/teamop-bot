4f2638247debb4a376f1d879d871b84542b3cbac
Rob Mehew <rob@graphitas.co.uk>
2018-05-02 17:36:24 +0100

    Adding a duel command

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
-  client.user.setActivity('Serving TeamOP');
+  client.user.setActivity('teamoverpowered.com');
 });
 
 client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  // COMMANDS
-  if(command === "duel") {	
-    const m = await message.channel.send("Dueling...");
-    m.edit(message.mentions.members.first() + " was slain by " + message.author);
-  }
+	if(message.author.bot) return;
+	if(message.content.indexOf(config.prefix) !== 0) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	// COMMANDS
+	switch(command) {	
+		case "duel":
+			const m = await message.channel.send("Dueling...");
+			m.edit(message.mentions.members.first() + " was slain by " + message.author);
+			break;
+		default:
+			await message.channel.send("This command is not recognized.");
+			break;
+	}
   
 });
 
