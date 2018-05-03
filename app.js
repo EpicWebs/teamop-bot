cf4954711578e105ea9131208f9ae45a78f7f14c
Rob Mehew <rob@graphitas.co.uk>
2018-05-03 16:30:34 +0100

    Adding command

--- app.js
+++ app.js
@@ -34,11 +34,13 @@ client.on("message", async message => {
 			break;
 		case "live":
 			var authorProfile = message.author.fetchProfile;
+			var authorConnections = authorProfile.connections;
 			
-			await message.channel.send("Live Command");
-		default:
-			await message.channel.send("This command is not recognized.");
-			break;
+			authorConnections.forEach(function(value){
+				var thisConnection = value;
+				
+				await message.channel.send(thisConnection.type);
+			});
 	}
   
 });
